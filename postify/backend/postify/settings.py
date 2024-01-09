5202f325ff5203788e8ed6fb6be2d85234738dcb
João Filipe <joaofilipeb045@gmail.com>
2024-01-09 12:26:48 -0300

    Config SQLite

--- postify/backend/postify/settings.py
+++ postify/backend/postify/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
-from datetime import timedelta
 import os
+from datetime import timedelta
+from pathlib import Path
+
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,12 +86,8 @@ WSGI_APPLICATION = "postify.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # Localização do arquivo do banco de dados
     }
 }
 
