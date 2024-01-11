eb9f2e727aa6b1431932a22d1349a7cbb17e74d3
João Filipe <joaofilipeb045@gmail.com>
2024-01-10 22:08:42 -0300

    Criando models e migrations

--- postify/backend/postify/urls.py
+++ postify/backend/postify/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from api.views import PostViewSet, UserViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
@@ -26,4 +28,4 @@ router.register(r"posts", PostViewSet, basename="post")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include((router.urls, "api"), namespace="postify")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
