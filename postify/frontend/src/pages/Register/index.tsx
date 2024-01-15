001846d8db6740775ac06dab32026cc1e6328314
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-01-15 09:36:40 -0300

    Adicionando POST de cadastro sem tratamento de erro

--- postify/frontend/src/pages/Register/index.tsx
+++ postify/frontend/src/pages/Register/index.tsx
@@ -1,16 +1,87 @@
 import { Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Cadastro = () => {
   const navigate = useNavigate();
+  const URL = "http://127.0.0.1:8000"
 
-  function handleCadastro() {
+  const [pessoa, setPessoa] = useState<{ nome: string, email: string, telefone: string, login: string, senha: string, repeteSenha: string }>({
+    nome: "", email: "", telefone: "", login: "", senha: "", repeteSenha: ""
+  });
 
-  }
+  const handleChance = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name == "nome") {
+      setPessoa({
+        ...pessoa,
+        nome: value
+      })
+    } else if (name == "email") {
+      setPessoa({
+        ...pessoa,
+        email: value
+      })
+    } else if (name == "telefone") {
+      setPessoa({
+        ...pessoa,
+        telefone: value
+      })
+    } else if (name == "loginDeUsuario") {
+      setPessoa({
+        ...pessoa,
+        login: value
+      })
+    } else if (name == "senha") {
+      setPessoa({
+        ...pessoa,
+        senha: value
+      })
+    } else if (name == "repeat_senha") {
+      setPessoa({
+        ...pessoa,
+        repeteSenha: value
+      })
+    }
+  };
 
-  const handleConfirm = () => {
-    alert("Cadastro efetuado com sucesso!");
-    return navigate("/")
+  const handleCadastro = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (pessoa.senha != pessoa.repeteSenha) {
+      alert("Senhas não compativeis")
+      return;
+    }
+
+    const data = {
+      "name": pessoa.nome,
+      "username": pessoa.login,
+      "email": pessoa.email,
+      "password": pessoa.senha,
+      "phone_number": pessoa.telefone
+    };
+
+    try {
+      const resposta = await fetch(
+        URL + "/api/v1/register/", {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }
+      );
+      console.log(data)
+      if (!resposta.ok) {
+        alert("Erro ao fazer cadastro, por favor, verifique se os dados estão corretos e tente novamente.");
+        throw new Error(`HTTP error! status: ${resposta.status}`);
+      }
+
+      alert("Cadastro efetuado com sucesso!")
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+
+    //return navigate('/')
   }
 
   const handleCancele = () => {
@@ -27,55 +98,61 @@ const Cadastro = () => {
           <TextField
             required
             type="text"
-            id="nome"
+            name="nome"
             variant="outlined"
             label="Nome Completo"
             margin='normal'
+            onChange={handleChance}
             fullWidth
           />
           <TextField
             required
             type="text"
-            id="email"
+            name="email"
             variant="outlined"
             label="Email"
             margin='normal'
+            onChange={handleChance}
             fullWidth
           />
           <TextField
             required
             type="number"
-            id="telefone"
+            name="telefone"
             variant="outlined"
             label="Telefone"
             margin='normal'
+            onChange={handleChance}
             fullWidth
           />
           <TextField
             required
             type="text"
-            id="loginDeUsuario"
+            name="loginDeUsuario"
             variant="outlined"
             label="Login de usuário"
             margin='normal'
+            onChange={handleChance}
             fullWidth
           />
           <TextField
             required
             type="password"
-            id="senha"
+            name="senha"
             variant="outlined"
             label="Senha"
             margin='normal'
+            onChange={handleChance}
             fullWidth
           />
           <TextField
             required
             type="password"
-            id="repeat_senha"
+            name="repeat_senha"
             variant="outlined"
             label="Repetir senha"
             margin='normal'
+            onChange={handleChance}
             fullWidth
           />
           <Grid container display={"flex"} justifyContent={"space-around"} marginTop={'20px'}>
@@ -85,7 +162,7 @@ const Cadastro = () => {
               </Button>
             </Grid>
             <Grid item>
-              <Button variant="contained" color="primary" onClick={handleConfirm}>
+              <Button variant="contained" color="primary" type="submit">
                 Confirmar
               </Button>
             </Grid>
