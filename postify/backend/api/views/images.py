ac086ded82c4c6088d3d5cafac48f0b1ae215348
matheus do vale <matheusvale0202@gmail.com>
2024-01-17 17:41:13 -0300

    add serializer and view

--- postify/backend/api/views/images.py
+++ postify/backend/api/views/images.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
 from ..models import Image
+from ..serializers import ImageSerializer
 
 
 class ImageViewSet(ViewSet):
@@ -23,7 +24,9 @@ class ImageViewSet(ViewSet):
         Get all posts active.
         GET /api/v1/posts/
         """
-        pass
+        images = self.get_all_images()
+        data = ImageSerializer(images, many=True).data
+        return Response(data=data, status=status.HTTP_200_OK)
 
     @transaction.atomic
     def create(self, request):
@@ -31,14 +34,30 @@ class ImageViewSet(ViewSet):
         Create image.
         POST /api/v1/images/
         """
-        pass
+        image = request.data
+        
+        image_serializer = ImageSerializer(data=image)
+        if image_serializer.is_valid():
+            image = image_serializer.save()
+        else:
+            return Response(
+                data=image_serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        return Response(status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         """
         Get a image.
         GET /api/v1/images/pk/
         """
-        pass
+        image = self.get_a_image(pk)
+        if image.exists():
+            data = ImageSerializer(image.first()).data
+            return Response(data=data, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"message": "Image not found."}, status=status.HTTP_404_NOT_FOUND
+            )
 
     @transaction.atomic
     def destroy(self, request, pk=None):
@@ -46,4 +65,12 @@ class ImageViewSet(ViewSet):
         Delete image.
         DELETE /api/v1/images/pk/
         """
-        pass
+        image = self.get_a_image(pk)
+        if image.exists():
+            image = image.get()
+            image.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(
+                {"message": "Image not found."}, status=status.HTTP_404_NOT_FOUND
+            )
