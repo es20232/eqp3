6ff63264c930d1c1159f81222e84752920b25d38
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-01-17 19:22:44 -0300

    Estilização da menuBar

--- postify/frontend/src/pages/menuBar.tsx
+++ postify/frontend/src/pages/menuBar.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import { Avatar } from '@mui/material';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { Avatar, Divider, List, ListItem, ListItemButton, ListItemText} from "@mui/material";
+
+const navItems = ["Feed", "About", "Contact"];
 
 const MenuBar = () => {
   const [auth, setAuth] = React.useState(true);
@@ -21,14 +23,31 @@ const MenuBar = () => {
   };
 
   return (
+    <div id="menuBar">
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left' }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ textAlign: "left" }}
+          >
             Postify
           </Typography>
+          <Divider />
+          <List>
+            <ListItem>
+              {navItems.map((item) => (
+                <ListItem key={item} disablePadding>
+                  <ListItemButton sx={{ textAlign: "center" }}>
+                    <ListItemText primary={item} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </ListItem>
+          </List>
           {auth && (
-            <div>
+            <div style={{marginLeft:'auto'}}>
               <IconButton
                 size="large"
                 aria-label="account of current user"
@@ -37,32 +56,33 @@ const MenuBar = () => {
                 onClick={handleMenu}
                 color="inherit"
               >
-                <Avatar></Avatar>
+                <Avatar />
               </IconButton>
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                  vertical: "top",
+                  vertical: "bottom",
                   horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
                   vertical: "top",
-                  horizontal: "right",
+                  horizontal: "left",
                 }}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Perfil</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
               </Menu>
             </div>
           )}
         </Toolbar>
       </AppBar>
     </Box>
+    </div>
   );
-}
+};
 
 export default MenuBar;
