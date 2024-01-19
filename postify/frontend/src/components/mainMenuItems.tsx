3bdb2aba66d34e5766e311bd8819cd2ada5219b6
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-01-18 21:48:21 -0300

    Propriedades dos botões do menu

---
+++ postify/frontend/src/components/mainMenuItems.tsx
@@ -0,0 +1,24 @@
+import HomeIcon from '@mui/icons-material/Home';
+import PublishIcon from '@mui/icons-material/Publish';
+import SaveIcon from '@mui/icons-material/Save';
+
+export const mainMenuItems = [
+    {
+        id: 0,
+        label: 'Início',
+        icon: <HomeIcon fontSize='large'/>,
+        route: '/home'
+    },
+    {
+        id: 1,
+        label: 'Postar',
+        icon: <PublishIcon fontSize='large'/>,
+        route: 'post'
+    },
+    {
+        id: 2,
+        label: 'Salvar Imagem',
+        icon: <SaveIcon fontSize='large'/>,
+        route: 'save'
+    },
+]
