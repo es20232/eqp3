2af8bf3fe5067583832e3d21d2d0b3671b50c154
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-01-22 13:53:57 -0300

    Inseriindo limite max nos campos

--- postify/frontend/src/utils/schemas/loginSchema.ts
+++ postify/frontend/src/utils/schemas/loginSchema.ts
@@ -2,8 +2,10 @@ import { z } from 'zod'
 
 export const loginSchema = z
   .object({
-    username: z.string().min(1, { message: 'O login não pode estar vazia.' }),
-    password: z.string().min(1, { message: 'A senha não pode estar vazia.' }),
+    username: z.string().min(1, { message: 'O login não pode estar vazia.' })
+      .max(50, { message: "Este campo não pode conter mais que 50 caracteres" }),
+    password: z.string().min(1, { message: 'A senha não pode estar vazia.' })
+      .max(30, { message: "Este campo não pode conter mais que 30 caracteres" }),
   })
   .required()
 
