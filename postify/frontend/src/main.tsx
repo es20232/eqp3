3757c8263560ae82d7e4033406c34d75820f6e44
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-02-09 14:59:40 -0300

    Criação da rota do Feed

--- postify/frontend/src/main.tsx
+++ postify/frontend/src/main.tsx
@@ -1,21 +1,21 @@
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import theme from './components/theme'
-import './index.css'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import theme from "./components/theme";
+import "./index.css";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from '../src/App.tsx'
-import Feed from './pages/Feed/index.tsx'
-import PrivateRoute from './components/PrivateRoute.tsx'
-import LoginPage from './pages/Login/index.tsx'
-import Register from './pages/Register/index.tsx'
-import { EditProfile, Profile } from './pages/UserProfile/index.tsx'
-import ErrorNotFound from './pages/errors/notFound.tsx'
-import { ApiConfig } from './utils/api/api.ts'
+import Home from "../src/App.tsx";
+import Feed from "./pages/Feed/index.tsx";
+import PrivateRoute from "./components/PrivateRoute.tsx";
+import LoginPage from "./pages/Login/index.tsx";
+import Register from "./pages/Register/index.tsx";
+import { EditProfile, Profile } from "./pages/UserProfile/index.tsx";
+import ErrorNotFound from "./pages/errors/notFound.tsx";
+import { ApiConfig } from "./utils/api/api.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme()}>
       <CssBaseline />
@@ -28,23 +28,23 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             errorElement={<ErrorNotFound />}
           />
           <Route
-            path={'/register'}
+            path={"/register"}
             element={<Register />}
             errorElement={<ErrorNotFound />}
           />
-          
 
           <Route path="/" element={<PrivateRoute />}>
             <Route
               path="/home"
               element={<Home />}
               errorElement={<ErrorNotFound />}
-            />
-            <Route 
-              path="/feed" 
-              element={<Feed />} 
-              errorElement={<ErrorNotFound />} />
-
+            >
+              <Route
+                index
+                element={<Feed />}
+                errorElement={<ErrorNotFound />}
+              />
+            </Route>
             <Route path="/profile" element={<Home />}>
               <Route
                 index
@@ -61,5 +61,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
