dfabafbb2e419f37dcf0d5f1def79fdce483ebe7
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-02-16 16:36:47 -0300

    interface de alterar senha da funcionalidade de recuperação de senha

--- postify/frontend/src/components/Auth/PasswordRecovery.tsx
+++ postify/frontend/src/components/Auth/PasswordRecovery.tsx
@@ -1,10 +1,34 @@
+import { Button, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
 import MyContainer from '../MyContainer';
 
 const passwordRecovery: React.FC = () => {
   return (
     <MyContainer>
-      teste
+      <Grid container>
+        <Grid item xs={12}><Typography variant="h6" textAlign={'center'}>Alterar senha</Typography></Grid>
+        <Grid item xs={12}>
+          <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <TextField
+              id="password"
+              label="senha"
+              placeholder="exemplo"
+              type="password"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              id="repeatPassword"
+              label="repetir senha"
+              placeholder="exemplo"
+              type="password"
+              margin="normal"
+              fullWidth
+            />
+            <Button type="submit" variant="contained">Confirmar</Button>
+          </form>
+        </Grid>
+      </Grid>
     </MyContainer>
   );
 }
