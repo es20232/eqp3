c60e6756c23d5d1ee317001d6c45d09f180c0fce
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-02-16 12:30:30 -0300

    adicionando verificação de erros ao campo

--- postify/frontend/src/components/Auth/Email/SubmitRequest.tsx
+++ postify/frontend/src/components/Auth/Email/SubmitRequest.tsx
@@ -1,5 +1,8 @@
-import { Grid } from "@mui/material";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { submitRequestFormData, submitRequestSchema } from "../../../utils/schemas/SubmitRequestSchema";
 import MyContainer from '../../MyContainer';
 
 
@@ -8,10 +11,34 @@ interface submitRequestParams {
 }
 
 const submitRequest: React.FC<submitRequestParams> = ({ type }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError
+  } = useForm<submitRequestFormData>({
+    resolver: zodResolver(submitRequestSchema),
+  })
+
   return (
     <MyContainer>
-      <Grid>
-        {type}
+      <Grid container>
+        <Grid item xs={12}><Typography variant="h6" textAlign={'center'}>Recuperação de senha</Typography></Grid>
+        <Grid item xs={12} display={'flex'} alignItems={'center'}>
+          <TextField
+            fullWidth
+            type="text"
+            variant="outlined"
+            label="Email"
+            placeholder='ex: teste@gmail.com'
+            {...register('email')}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+          <Button variant='contained' type='submit' sx={{ marginLeft: '10px' }}>
+            Enviar
+          </Button>
+        </Grid>
       </Grid>
     </MyContainer>
   );
