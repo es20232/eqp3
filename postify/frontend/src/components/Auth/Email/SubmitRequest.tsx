e510f4266365659775d3a38c94b58845b6741f62
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-02-16 16:05:12 -0300

    algumas configurações de envio de formulario com tratamento de erros do campo

--- postify/frontend/src/components/Auth/Email/SubmitRequest.tsx
+++ postify/frontend/src/components/Auth/Email/SubmitRequest.tsx
@@ -1,10 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { submitRequestFormData, submitRequestSchema } from "../../../utils/schemas/SubmitRequestSchema";
 import MyContainer from '../../MyContainer';
-
 
 interface submitRequestParams {
   type: ('PASSWORDRECOVERY' /*| 'mais coisas'*/);
@@ -14,30 +13,39 @@ const submitRequest: React.FC<submitRequestParams> = ({ type }) => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-    setError
+    formState: { errors }
   } = useForm<submitRequestFormData>({
     resolver: zodResolver(submitRequestSchema),
   })
+
+  const onSubmit: SubmitHandler<submitRequestFormData> = async (data) => {
+    console.log(data)
+  }
 
   return (
     <MyContainer>
       <Grid container>
         <Grid item xs={12}><Typography variant="h6" textAlign={'center'}>Recuperação de senha</Typography></Grid>
-        <Grid item xs={12} display={'flex'} alignItems={'center'}>
-          <TextField
-            fullWidth
-            type="text"
-            variant="outlined"
-            label="Email"
-            placeholder='ex: teste@gmail.com'
-            {...register('email')}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-          />
-          <Button variant='contained' type='submit' sx={{ marginLeft: '10px' }}>
-            Enviar
-          </Button>
+        <Grid item xs={12}>
+          <form onSubmit={handleSubmit(onSubmit)} autoComplete="off" style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <TextField
+              fullWidth
+              type="text"
+              variant="outlined"
+              label="Email"
+              placeholder='ex: teste@gmail.com'
+              {...register('email')}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+            <Button variant='contained' type='submit' sx={{ marginLeft: '10px' }}>
+              Enviar
+            </Button>
+          </form>
         </Grid>
       </Grid>
     </MyContainer>
--- postify/frontend/src/components/Auth/PasswordRecovery.tsx
+++ postify/frontend/src/components/Auth/PasswordRecovery.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import MyContainer from '../MyContainer';
 
 const passwordRecovery: React.FC = () => {
   return (
-    <>
-    </>
+    <MyContainer>
+      teste
+    </MyContainer>
   );
 }
 
