cf488834307b6e8ad1da8543e046f7625b3040bf
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-02-17 18:08:46 -0300

    tratamento de erros da funcionalidade de recuperação de senha

--- postify/frontend/src/components/Auth/PasswordRecovery.tsx
+++ postify/frontend/src/components/Auth/PasswordRecovery.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Grid, TextField, Typography } from '@mui/material'
+import { AlertColor, Button, Grid, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useNavigate, useParams } from 'react-router-dom'
 import { api } from '../../utils/api/api'
@@ -7,6 +8,7 @@ import {
   RecoveryPasswordFormData,
   RecoveryPasswordSchema,
 } from '../../utils/schemas/RecoveryPasswordSchema'
+import AlertInformativo from '../AlertInformativo'
 import MyContainer from '../MyContainer'
 
 const PasswordRecovery = () => {
@@ -15,11 +17,27 @@ const PasswordRecovery = () => {
   const {
     register: registerPassword,
     handleSubmit: submitPassword,
-    reset: resetSenhas,
     formState: { errors: errorsPassword },
   } = useForm<RecoveryPasswordFormData>({
     resolver: zodResolver(RecoveryPasswordSchema),
   })
+
+  const [status, setStatus] = useState<{ erro: boolean, sucess: boolean, message: string }>({
+    erro: false, sucess: false, message: ''
+  });
+
+  const alert = () => {
+    var color: AlertColor;
+    if (status.erro) color = 'error'
+    else color = 'success'
+
+    return (
+      <AlertInformativo
+        message={status.message}
+        severityMessage={color}
+      />
+    );
+  }
 
   const onSubmit: SubmitHandler<RecoveryPasswordFormData> = async (data) => {
     await api
@@ -28,56 +46,75 @@ const PasswordRecovery = () => {
       })
       .then((response) => {
         console.log(response)
-        navigate('/')
+        setStatus({
+          ...status,
+          sucess: true,
+          message: response?.data.message
+        })
+        setTimeout(() => {
+          navigate('/')
+        }, 3000)
       })
-      .catch((error) => console.log(error))
+      .catch((error) => {
+        setStatus({
+          ...status,
+          erro: true,
+          message: 'Houve algum erro ao tentar alterar a senha'
+        })
+        setTimeout(() => {
+          navigate('/')
+        }, 5000)
+      })
   }
 
   return (
-    <MyContainer>
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography variant="h6" textAlign={'center'}>
-            Alterar senha
-          </Typography>
+    <React.Fragment>
+      {(status.erro || status.sucess) && alert()}
+      <MyContainer>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="h6" textAlign={'center'}>
+              Alterar senha
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <form
+              onSubmit={submitPassword(onSubmit)}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <TextField
+                label="senha"
+                placeholder="exemplo"
+                type="password"
+                margin="normal"
+                fullWidth
+                {...registerPassword('password')}
+                error={!!errorsPassword.password}
+                helperText={errorsPassword.password?.message}
+              />
+              <TextField
+                label="repetir senha"
+                placeholder="exemplo"
+                type="password"
+                margin="normal"
+                fullWidth
+                {...registerPassword('repeatPassword')}
+                error={!!errorsPassword.repeatPassword}
+                helperText={errorsPassword.repeatPassword?.message}
+              />
+              <Button type="submit" variant="contained" disabled={(status.erro || status.sucess)}>
+                Confirmar
+              </Button>
+            </form>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <form
-            onSubmit={submitPassword(onSubmit)}
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <TextField
-              label="senha"
-              placeholder="exemplo"
-              type="password"
-              margin="normal"
-              fullWidth
-              {...registerPassword('password')}
-              error={!!errorsPassword.password}
-              helperText={errorsPassword.password?.message}
-            />
-            <TextField
-              label="repetir senha"
-              placeholder="exemplo"
-              type="password"
-              margin="normal"
-              fullWidth
-              {...registerPassword('repeatPassword')}
-              error={!!errorsPassword.repeatPassword}
-              helperText={errorsPassword.repeatPassword?.message}
-            />
-            <Button type="submit" variant="contained">
-              Confirmar
-            </Button>
-          </form>
-        </Grid>
-      </Grid>
-    </MyContainer>
+      </MyContainer>
+    </React.Fragment>
   )
 }
 
