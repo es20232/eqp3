a345cd1d37e63641ec60d3bcc4eb12a9ea2c54fa
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-02-17 18:30:14 -0300

    inserindo tratamento de erro ao SubmitRequest

--- postify/frontend/src/components/Auth/Email/SubmitRequest.tsx
+++ postify/frontend/src/components/Auth/Email/SubmitRequest.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Grid, TextField, Typography } from '@mui/material'
+import { AlertColor, Button, Grid, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import { api } from '../../../utils/api/api'
@@ -7,6 +8,7 @@ import {
   submitRequestFormData,
   submitRequestSchema,
 } from '../../../utils/schemas/SubmitRequestSchema'
+import AlertInformativo from '../../AlertInformativo'
 import MyContainer from '../../MyContainer'
 
 const SubmitRequest = () => {
@@ -18,6 +20,22 @@ const SubmitRequest = () => {
   } = useForm<submitRequestFormData>({
     resolver: zodResolver(submitRequestSchema),
   })
+  const [status, setStatus] = useState<{ erro: boolean, sucess: boolean, message: string }>({
+    erro: false, sucess: false, message: ''
+  });
+
+  const alert = () => {
+    var color: AlertColor;
+    if (status.erro) color = 'error'
+    else color = 'success'
+
+    return (
+      <AlertInformativo
+        message={status.message}
+        severityMessage={color}
+      />
+    );
+  }
 
   const onSubmit: SubmitHandler<submitRequestFormData> = async (data) => {
     console.log(data)
@@ -26,51 +44,64 @@ const SubmitRequest = () => {
         email: data.email,
       })
       .then((response) => {
-        console.log(response)
-        navigate('/verified-email')
+        setStatus({
+          ...status,
+          sucess: true, message: response.data.message
+        })
+        setTimeout(() => {
+          navigate('/')
+        }, 3000)
       })
-      .catch((error) => console.log(error))
+      .catch((error) => {
+        setStatus({
+          ...status,
+          erro: true, message: 'e-mail não enviado. Por favor, insira um email válido ou tente novamente mais tarde.'
+        })
+      })
   }
 
   return (
-    <MyContainer>
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography variant="h6" textAlign={'center'}>
-            Recuperação de senha
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            autoComplete="off"
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <TextField
-              fullWidth
-              type="text"
-              variant="outlined"
-              label="Email"
-              placeholder="ex: teste@gmail.com"
-              {...register('email')}
-              error={!!errors.email}
-              helperText={errors.email?.message}
-            />
-            <Button
-              variant="contained"
-              type="submit"
-              sx={{ marginLeft: '10px' }}
+    <React.Fragment>
+      {(status.erro || status.sucess) && alert()}
+      <MyContainer>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="h6" textAlign={'center'}>
+              Recuperação de senha
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              autoComplete="off"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
             >
-              Enviar
-            </Button>
-          </form>
+              <TextField
+                fullWidth
+                type="text"
+                variant="outlined"
+                label="Email"
+                placeholder="ex: teste@gmail.com"
+                {...register('email')}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+              <Button
+                variant="contained"
+                type="submit"
+                sx={{ marginLeft: '10px' }}
+              >
+                Enviar
+              </Button>
+            </form>
+          </Grid>
         </Grid>
-      </Grid>
-    </MyContainer>
+      </MyContainer>
+    </React.Fragment>
   )
 }
 
