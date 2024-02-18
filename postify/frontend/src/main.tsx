6da06cf73356034a9290047292bb62d2fca571ad
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-02-18 19:38:54 -0300

    Criação da rota para a página de criação do post

--- postify/frontend/src/main.tsx
+++ postify/frontend/src/main.tsx
@@ -1,27 +1,28 @@
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import theme from './components/theme'
-import './index.css'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import theme from "./components/theme";
+import "./index.css";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from '../src/App.tsx'
+import Home from "../src/App.tsx";
 import {
   PasswordRecovery,
   SubmitRequest,
   VerifiedEmail,
-} from './components/Auth/index.ts'
-import PrivateRoute from './components/PrivateRoute.tsx'
-import Feed from './pages/Feed/index.tsx'
-import LoginPage from './pages/Login/index.tsx'
-import Register from './pages/Register/index.tsx'
-import { EditProfile, Profile } from './pages/UserProfile/index.tsx'
-import ErrorNotFound from './pages/errors/notFound.tsx'
-import { ApiConfig } from './utils/api/api.ts'
+} from "./components/Auth/index.ts";
+import PrivateRoute from "./components/PrivateRoute.tsx";
+import Feed from "./pages/Feed/index.tsx";
+import LoginPage from "./pages/Login/index.tsx";
+import Register from "./pages/Register/index.tsx";
+import { EditProfile, Profile } from "./pages/UserProfile/index.tsx";
+import ErrorNotFound from "./pages/errors/notFound.tsx";
+import { ApiConfig } from "./utils/api/api.ts";
 import Search from "./pages/Search/index.tsx";
+import Post from "./pages/Post/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme()}>
       <CssBaseline />
@@ -29,14 +30,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BrowserRouter basename="/">
         <Routes>
           <Route index element={<LoginPage />} />
-          <Route path={'*'} element={<ErrorNotFound />} />
-          <Route path={'/register'} element={<Register />} />
-          <Route path={'/password-recovery'} element={<SubmitRequest />} />
+          <Route path={"*"} element={<ErrorNotFound />} />
+          <Route path={"/register"} element={<Register />} />
+          <Route path={"/password-recovery"} element={<SubmitRequest />} />
           <Route
-            path={'/password-recovery/change/:token'}
+            path={"/password-recovery/change/:token"}
             element={<PasswordRecovery />}
           />
-          <Route path={'/verified-email'} element={<VerifiedEmail />} />
+          <Route path={"/verified-email"} element={<VerifiedEmail />} />
 
           <Route path="/" element={<PrivateRoute />}>
             <Route
@@ -51,26 +52,23 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
               />
             </Route>
             <Route path="/profile" element={<Home />}>
-              <Route
-                index
-                element={<Profile />}
-              />
-              <Route
-                path="/profile/edit"
-                element={<EditProfile />}
-              />
+              <Route index element={<Profile />} />
+              <Route path="/profile/edit" element={<EditProfile />} />
               <Route path=":username" element={<Profile />} />
             </Route>
             <Route path="/search" element={<Home />}>
-            <Route
+              <Route
                 index
-                element={<Search/>}
-                errorElement={<ErrorNotFound/>}
+                element={<Search />}
+                errorElement={<ErrorNotFound />}
               />
+            </Route>
+            <Route path="/createPost" element={<Home />}>
+              <Route index element={<Post/>} errorElement={<ErrorNotFound/>}/>
             </Route>
           </Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
