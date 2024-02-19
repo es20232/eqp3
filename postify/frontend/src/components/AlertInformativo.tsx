cfd711517797f853b66df0fef0017f43a0b23ef1
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-02-19 13:52:00 -0300

    inserindo parametros de posicionamento da janela informativa, mantendo o valor anterior como padrão

--- postify/frontend/src/components/AlertInformativo.tsx
+++ postify/frontend/src/components/AlertInformativo.tsx
@@ -3,13 +3,26 @@ import React, { useState } from 'react';
 
 interface AlertParams {
   message: string;
-  severityMessage: AlertColor;//severity type
+  severityMessage: AlertColor;
+  alignVertical?: "bottom" | "top";
+  alignHorizontal?: "left" | "center" | "right";
 }
 
-const AlertSnackBar = ({ message, severityMessage }: AlertParams) => {
+/**
+ * 
+ * @param message informação a ser passada ao usuário
+ * @param severityMessage tipo de mensagem que será passada. ex: erro, sucess
+ * @param alignVertical localização da janela informativa na vertical
+ * @param alignHorizontal localização da janela informativa na Horizontal
+ * @returns
+ */
+const AlertSnackBar: React.FC<AlertParams> = ({ message, severityMessage, alignVertical, alignHorizontal }) => {
+  if (alignVertical === undefined)
+    alignVertical = 'top';
+  if (alignHorizontal === undefined)
+    alignHorizontal = 'center';
 
   const [open, setOpen] = useState(true);
-
 
   const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
     if (reason === 'clickaway') {
@@ -26,8 +39,8 @@ const AlertSnackBar = ({ message, severityMessage }: AlertParams) => {
         autoHideDuration={4000}
         onClose={handleClose}
         anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center'
+          vertical: alignVertical,
+          horizontal: alignHorizontal,
         }}
       >
         <Alert severity={severityMessage} sx={{ width: '100%' }}>
