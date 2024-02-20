8572b344fa76c991b87498877ef388fd6f1928f0
Ytallo16 <ytallogomes126@gmail.com>
2024-02-20 00:55:25 -0300

    a legenda morreu

--- postify/frontend/src/pages/Feed/Comentario.tsx
+++ postify/frontend/src/pages/Feed/Comentario.tsx
@@ -1,9 +1,19 @@
-import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { api } from "../../utils/api/api";
-import formatDate from "../../utils/date/format";
+import DeleteIcon from '@mui/icons-material/Delete'
+import {
+  Avatar,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { api } from '../../utils/api/api'
+import formatDate from '../../utils/date/format'
 
-const API = 'http://localhost:8000'
+const API = 'http://localhost:8000' // Substitua 'http://example.com' pelo URL correto da sua API
 
 interface User {
   id: number
@@ -22,30 +32,43 @@ interface Comment {
 }
 
 interface ComentarioParam {
-  idPost: number;
+  idPost: number
 }
 
 const Comentario: React.FC<ComentarioParam> = ({ idPost }) => {
-  const [carregamentoInicial, setCarregamentoInicial] = useState(true);
-  const [comentarios, setComentarios] = useState<Comment[]>();
+  const [carregamentoInicial, setCarregamentoInicial] = useState(true)
+  const [comentarios, setComentarios] = useState<Comment[]>()
 
   const handleGetComentarios = async () => {
-    await api.get(`api/v1/posts/${idPost}/comments`)
+    await api
+      .get(`api/v1/posts/${idPost}/comments`)
       .then((response) => {
         setComentarios(response.data)
+      })
+      .catch((error) => {
+        console.error('Erro ao obter comentários:', error)
       })
   }
 
   useEffect(() => {
     if (carregamentoInicial) {
-      handleGetComentarios();
-      setCarregamentoInicial(false);
+      handleGetComentarios()
+      setCarregamentoInicial(false)
     } else {
-      setTimeout(() => {
-        handleGetComentarios();
-      }, 10000)
+      const intervalId = setInterval(handleGetComentarios, 10000)
+
+      return () => clearInterval(intervalId)
     }
-  })
+  }, [carregamentoInicial]) // Corrigido para adicionar carregamentoInicial como dependência do useEffect
+
+  const handleDeleteComment = async (idComment: number) => {
+    try {
+      await api.delete(`api/v1/comments/${idComment}`)
+      handleGetComentarios()
+    } catch (error) {
+      console.error('Erro ao excluir comentário:', error)
+    }
+  }
 
   return (
     <React.Fragment>
@@ -55,36 +78,48 @@ const Comentario: React.FC<ComentarioParam> = ({ idPost }) => {
       <List
         sx={{ maxHeight: '200px', overflow: 'auto', border: `1px solid black` }}
       >
-        {comentarios !== undefined && comentarios.map((comment) => (
-          <ListItem key={comment.id} sx={{ display: 'flex', flexDirection: 'column' }}>
-            <Box sx={{ display: 'flex', width: '100%' }}>
-              <ListItemAvatar sx={{ display: 'flex', alignItems: 'center' }}>
-                <Avatar alt={comment.user.name} src={API + comment.user.profile_image} />
-              </ListItemAvatar>
-              <ListItemText>
-                <Typography
-                  variant="subtitle1"
-                  sx={{ fontWeight: 'bold' }}
-                >
-                  {comment.user.username}
-                </Typography>
-                <Typography variant="body2">
-                  {
-                    comment.created_at instanceof Date
-                      ? formatDate(comment.created_at.toUTCString())
-                      : formatDate(new Date(comment.created_at).toUTCString())
-                  }
-                </Typography>
+        {comentarios !== undefined &&
+          comentarios.map((comment) => (
+            <ListItem
+              key={comment.id}
+              sx={{ display: 'flex', flexDirection: 'column' }}
+            >
+              <Box sx={{ display: 'flex', width: '100%' }}>
+                <ListItemAvatar sx={{ display: 'flex', alignItems: 'center' }}>
+                  <Avatar
+                    alt={comment.user.name}
+                    src={API + comment.user.profile_image}
+                  />
+                </ListItemAvatar>
+                <ListItemText>
+                  <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>
+                    {comment.user.username}
+                  </Typography>
+                  <Typography variant="body2">
+                    {formatDate(comment.created_at)}
+                  </Typography>
+                </ListItemText>
+              </Box>
+              <ListItemText
+                sx={{
+                  height: '100%',
+                  width: '100%',
+                  borderBottom: '0.3px solid gray',
+                }}
+              >
+                <Typography textAlign={'justify'}>{comment.comment}</Typography>
               </ListItemText>
-            </Box>
-            <ListItemText sx={{ height: '100%', width: '100%', borderBottom: '0.3px solid gray' }}>
-              <Typography textAlign={'justify'}>{comment.comment}</Typography>
-            </ListItemText>
-          </ListItem>
-        ))}
+              <IconButton
+                onClick={() => handleDeleteComment(comment.id)}
+                size="small"
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItem>
+          ))}
       </List>
     </React.Fragment>
-  );
+  )
 }
 
-export default Comentario;
+export default Comentario
